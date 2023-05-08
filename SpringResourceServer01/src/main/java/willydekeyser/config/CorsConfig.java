15a382a2706076908f1722a54e47fb0f18351e46
willydekeyser <ma074172@skynet.be>
2023-05-08 13:00:14 +0200

    Update Corsconfig & Cloud Gateway

--- SpringResourceServer01/src/main/java/willydekeyser/config/CorsConfig.java
+++ SpringResourceServer01/src/main/java/willydekeyser/config/CorsConfig.java
@@ -1,23 +1,23 @@
 package willydekeyser.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class CorsConfig {
 
-	@Bean
-	WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-				.allowedOrigins("http://localhost:4200")
-				.allowedMethods("*")
-				.allowedHeaders("*");
-			}
-		};
-	}
+//	@Bean
+//	WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**")
+//				.allowedOrigins("http://localhost:4200")
+//				.allowedMethods("*")
+//				.allowedHeaders("*");
+//			}
+//		};
+//	}
 }
