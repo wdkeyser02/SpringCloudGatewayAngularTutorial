aed7af94bbe747479a85620b64156659da3c9e6c
willydekeyser <ma074172@skynet.be>
2023-05-08 12:49:50 +0200

    Setup Spring Resource Server 01 & Spring Resource Server 01

---
+++ SpringResourceServer02/src/main/java/willydekeyser/config/Corsconfig.java
@@ -0,0 +1,23 @@
+package willydekeyser.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Corsconfig {
+
+	@Bean
+	WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedOrigins("http://localhost:4200")
+				.allowedMethods("*")
+				.allowedHeaders("*");
+			}
+		};
+	}
+}
