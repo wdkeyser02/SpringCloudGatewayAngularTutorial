51007195088975d774a9d87dbcdb381a816ac6e6
willydekeyser <ma074172@skynet.be>
2023-06-06 15:47:37 +0200

    Update Gateway

--- SpringResourceServer01/src/main/java/willydekeyser/controller/Homecontroller.java
+++ SpringResourceServer01/src/main/java/willydekeyser/controller/Homecontroller.java
@@ -1,5 +1,7 @@
 package willydekeyser.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +13,11 @@ public class Homecontroller {
 	@GetMapping("/")
 	public String home() {
 		return "Spring Resource Server 01";
+	}
+	
+	@GetMapping("/me")
+	public String me() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		return auth.getName();
 	}
 }
